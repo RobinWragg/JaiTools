2e51667ad2774552ed7720c798ac83cacfcc6422
Robin Wragg <robin.wragg@me.com>
2019-11-22 01:45:41 +0000

    Remove experimental build system and .no-sublime-package file

--- JaiBuildSystemShim.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-print(sys.argv[0])
-current_jai_file = sys.argv[1]
-print('JaiTools: Building project...')
-print('JaiTools: No build.jai file found in the project.')
-print("JaiTools: Running 'jai " + current_jai_file + "'...")
