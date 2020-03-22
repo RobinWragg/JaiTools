1727e26ec35a6703cce669d6e0a033287199b161
Robin Wragg <robin.wragg@me.com>
2020-03-22 00:08:28 +0000

    SetBuildFile refactor

--- SetBuildFileCommand.py
+++ SetBuildFileCommand.py
@@ -12,9 +12,6 @@ class SetBuildFileHandler(sublime_plugin.ListInputHandler):
   def name(self):
     return 'build_file_path'
   
-  def placeholder(self):
-    return 'Which Jai file should be passed to the compiler?'
-  
   def list_items(self):
     return self.relative_file_paths
 
@@ -22,24 +19,22 @@ class SetJaiBuildFileCommand(sublime_plugin.WindowCommand):
   common_parent_dir = ''
   
   def input(self, args):
-    paths = get_all_jai_project_file_paths()
+    paths_set = get_all_jai_project_file_paths()
     
     # Add open files to the paths set, in case they're not part of the project
     for view in sublime.active_window().views():
       if view_is_jai(view) and view.file_name() != None:
-        paths.add(view.file_name())
+        paths_set.add(view.file_name())
     
-    paths = list(paths)
-    
-    if len(paths) == 0:
+    if len(paths_set) == 0:
       sublime.error_message('There are no Jai files open or in your project. Save a Jai file and try again.')
       return None
     
-    self.common_parent_dir = get_common_parent_dir(paths)
+    self.common_parent_dir = get_common_parent_dir(list(paths_set))
     
     relative_paths = []
     
-    for path in paths:
+    for path in paths_set:
       relative_paths.append(os.path.relpath(path, self.common_parent_dir))
       
     return SetBuildFileHandler(relative_paths)
