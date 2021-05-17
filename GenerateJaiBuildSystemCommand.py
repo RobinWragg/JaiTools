254edc9ab113f2a7865eaf6ecab2607b62fc9a0e
Robin Wragg <robin.wragg@me.com>
2021-05-17 18:39:56 +0100

    Added command for build system generation

--- GenerateJaiBuildSystemCommand.py
+++ GenerateJaiBuildSystemCommand.py
@@ -1,17 +1,40 @@
 import sublime
 import sublime_plugin
+import json
+import subprocess
 
 # rwtodo: don't forget to update the readme when this is done!
+# rwtodo: weirdness is happening with the order of stdout and stderr
 
-class GenerateJaiBuildSystemCommand(sublime_plugin.WindowCommand):
+class GenerateJaiBuildSystemCommand(sublime_plugin.ApplicationCommand):
   def run(self):
-    # check a .sublime-project file exists
-    # ask for build file
-    # if a build system exists, ask if we should overwrite it
-    # Copy the contents of Jai.sublime-build into the right spot, modifying the "shell_cmd" line
-    print('rwtodo')
+    window = sublime.active_window()
     
-    # rwtodo: Jai.sublime-build needs that syntax highlighting thing.
+    if window.project_file_name() == None:
+      sublime.error_message('Project file not found. Choose "Project > Save Project As..." and try again.')
+      return
+    
+    project = window.project_data()
+    
+    build_system = sublime.load_resource('Packages/JaiTools/Jai.sublime-build')
+    build_system = json.loads(build_system)
+    
+    build_system['name'] = 'Jai Project'
+    del build_system['selector']
+    
+    # rwtodo: ask for the build file and modify the 'shell_cmd' entry
+    
+    if 'build_systems' in project:
+      project['build_systems'].append(build_system)
+    else:
+      project['build_systems'] = [build_system]
+    
+    window.set_project_data(project)
+
+
+
+
+
 
 
 
