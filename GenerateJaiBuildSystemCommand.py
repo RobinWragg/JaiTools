4daa94a3fbbb287ca6a6966d59f9c716cc121691
Robin Wragg <robin.wragg@fxhome.com>
2021-05-27 21:24:32 +0100

    Build system notes

--- GenerateJaiBuildSystemCommand.py
+++ GenerateJaiBuildSystemCommand.py
@@ -22,7 +22,7 @@ class GenerateJaiBuildSystemCommand(sublime_plugin.ApplicationCommand):
     build_system['name'] = 'Jai Project'
     del build_system['selector']
     
-    # rwtodo: ask for the build file and modify the 'shell_cmd' entry
+    # rwtodo: ask for the build file and modify the 'shell_cmd' and 'working_dir' entries. make working_dir the project path, and use the full file path for *.jai with quotes.
     
     if 'build_systems' in project:
       project['build_systems'].append(build_system)
